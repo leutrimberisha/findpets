02093f164f3cee9ffa7baad5f60291eb2da19f85
leutrimberisha <45616565+leutrimberisha@users.noreply.github.com>
2023-12-25 03:15:01 +0100

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,11 +4,11 @@ import Logo from '../../assets/logo-white.png'
 const Navigation = () => {
     return (
         <nav>
-            <a href="/cadastro" className="items">😨 Perdi meu bichinho</a>
-            <a href="/cadastro" className="items">😱 Encontrei um bichinho</a>
+            <a href="/cadastro" className="items">😨 I lost my pet</a>
+            <a href="/cadastro" className="items">😱 I found a little animal</a>
             <a href="/pets"><img src={Logo} alt="Logo" /></a>
-            <a href="/cadastro" className="items">🥰 Adotar um animalzinho</a>
-            <a href="https://play.google.com/store/apps/details?id=com.kalifyinc.findyourpet&pli=1" target="_blank" rel="noreferrer" className="items">😍 Download do App (Android)</a>
+            <a href="/cadastro" className="items">🥰 Adopt a pet</a>
+            <a href="https://play.google.com/store/apps/details?id=com.kalifyinc.findyourpet&pli=1" target="_blank" rel="noreferrer" className="items">😍 Download App (Android)</a>
             {/* <a href="/maps" className="items">📌 Mapa dos Animais</a> */}
         </nav>
     )
